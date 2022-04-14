9bc10ff9457d023f8e7815cbba941f1d4a17ded3
maxsibilla <maxsibilla@icloud.com>
2022-04-14 15:52:08 -0400

    Removing logging from main

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
 import importlib
-import logging
 import os
 import sys
 from pathlib import Path
 
 from flask import Flask
 from yaml import safe_load
-
-# Set logging format and level (default is warning)
-# All the API logging is forwarded to the uWSGI server and gets written into the log file `uwsgo-entity-api.log`
-# Log rotation is handled via logrotate on the host system with a configuration file
-# Do NOT handle log file and rotation via the Python logging to avoid issues with multi-worker processes
-
-logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(module)s:%(lineno)d: %(message)s', level=logging.DEBUG,
-                    datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger(__name__)
 
 config = {}
 app = Flask(__name__, instance_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance'),
@@ -24,9 +14,6 @@ app.config.from_pyfile('app.cfg')
 # load the index configurations and set the default
 config['INDICES'] = safe_load((Path(__file__).absolute().parent / 'instance/search-config.yaml').read_text())
 config['DEFAULT_INDEX_WITHOUT_PREFIX'] = config['INDICES']['default_index']
-
-logger.debug("############ INDICES config LOADED")
-logger.debug(config['INDICES'])
 
 # Remove trailing slash / from URL base to avoid "//" caused by config with trailing slash
 config['DEFAULT_ELASTICSEARCH_URL'] = config['INDICES']['indices'][config['DEFAULT_INDEX_WITHOUT_PREFIX']]['elasticsearch']['url'].strip('/')
@@ -52,5 +39,4 @@ if __name__ == "__main__":
     except Exception as e:
         print("Error during starting debug server.")
         print(str(e))
-        search_api.logger.error(e, exc_info=True)
-        print("Error during startup check the log file for further information")
+        print("Error during startup.")
